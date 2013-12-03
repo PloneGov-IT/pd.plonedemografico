8623dbbf252bb717d422c0268dd3cd81c9148936
ale-rt <alessandro.pisa@redturtle.it>
2013-12-03 17:22:20 +0000

    vista conferma prenotazioni

--- pd/plonedemografico/browser/prenotazione_add.py
+++ pd/plonedemografico/browser/prenotazione_add.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from pd.prenotazioni.browser.prenotazione_add import AddForm as BaseForm
 from plone.app.form.validators import null_validator
 from rg.prenotazioni import prenotazioniMessageFactory as _
 from urllib import urlencode
-from zope.formlib.form import action
+from zope.formlib.form import action, setUpWidgets
 
 
 class AddForm(BaseForm):
     """ Customize pd.prenotazioni add form to redirect to another page
     """
+    template = ViewPageTemplateFile('prenotazione_add.pt')
+
     banned_redirect_keys = (
         '_authenticator',
         'ajax_include_head',
@@ -18,6 +21,24 @@ class AddForm(BaseForm):
         'form.captcha',
         'form.tipology-empty-marker',
     )
+
+    def setUpWidgets(self, ignore_request=False):
+        '''
+        From zope.formlib.form.Formbase.
+        '''
+        self.adapters = {}
+        fieldnames = [x.__name__ for x in self.form_fields]
+        data = {}
+        for key in fieldnames:
+            value = self.request.form.get(key)
+            if value is not None and not value == u'':
+                data[key] = value
+                self.request[key] = value
+
+        self.widgets = setUpWidgets(
+            self.form_fields, self.prefix, self.context, self.request,
+            form=self, adapters=self.adapters, ignore_request=ignore_request,
+            data=data)
 
     @action(_('action_book', u'Book'), name=u'book')
     def action_book(self, action, data):
