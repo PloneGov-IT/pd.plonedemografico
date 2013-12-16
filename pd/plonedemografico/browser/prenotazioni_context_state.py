50684449596c23e2f4b74994a434aa36d90739f9
ale-rt <alessandro.pisa@redturtle.it>
2013-12-16 17:02:40 +0000

    Again form cleaning

--- pd/plonedemografico/browser/prenotazioni_context_state.py
+++ pd/plonedemografico/browser/prenotazioni_context_state.py
@@ -20,4 +20,5 @@ class PrenotazioniContextState(BaseView):
         elif isinstance(add_view, list):
             add_view = add_view[0]
             self.request['form.add_view'] = add_view
+            self.request.form['form.add_view'] = add_view
         return add_view
