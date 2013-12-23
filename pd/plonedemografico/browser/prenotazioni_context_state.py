0f4ae8b184b7e4d4fed384e52fc30822c4cc8a93
ale-rt <alessandro.pisa@redturtle.it>
2013-12-23 16:43:46 +0000

    fix url problems

--- pd/plonedemografico/browser/prenotazioni_context_state.py
+++ pd/plonedemografico/browser/prenotazioni_context_state.py
@@ -17,8 +17,4 @@ class PrenotazioniContextState(BaseView):
         add_view = self.request.form.get('form.add_view', '')
         if not add_view:
             add_view = BaseView.add_view
-        elif isinstance(add_view, list):
-            add_view = add_view[0]
-            self.request['form.add_view'] = add_view
-            self.request.form['form.add_view'] = add_view
         return add_view
